6474a3371232e2b0b3285a5d44c91b0f6d553cba
hhfa008 <hhfa008@gmail.com>
2019-06-13 11:34:54 +0800

    Initial commit

---
+++ Example/SwiftUINow/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftUINow
+//
+//  Created by hhfa008 on 06/13/2019.
+//  Copyright (c) 2019 hhfa008. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
